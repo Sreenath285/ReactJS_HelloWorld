21f0b434112ba7655fe40dcf92bb23b7be6a1941
Sreenath285 <sreenath.285@gmail.com>
2022-04-24 20:05:05 +0530

    BridgeLabz site will open in new tab when image is clicked

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
+import logo from './assets/BL-logo.jpg';
 import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  url="https://www.bridgelabz.com/"
+  constructor() {
+    super() 
+    this.state = {
+      title: 'Hello from Bridgelabz'
+    }
+  }
+
+  /**
+   * onClick method is called when image is clicked
+   * here clicking is the event that we're checking
+   */
+  onClick = ($event) => {
+    window.open(this.url,"_blank");
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello from BridgeLabz</h1>
+        <h1>{this.state.title}</h1>
+        <img src={logo} onClick={this.onClick} 
+          alt="BridgeLabz Logo goes here."/>
       </div>
-    );
+    )
   }
-}
-
+} 
 export default App;
