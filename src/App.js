792d8d6b7f9c1b22b85477ffc53021a239548cef
Sreenath285 <sreenath.285@gmail.com>
2022-04-24 20:14:44 +0530

    Displaying hello from bridgelabz with a user input name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
   constructor() {
     super() 
     this.state = {
-      title: 'Hello from Bridgelabz'
+      //title: 'Hello from Bridgelabz',
+      userName: ''
     }
   }
 
@@ -19,13 +20,26 @@ class App extends React.Component {
     window.open(this.url,"_blank");
   }
 
+  /**
+   * onChange event listener is used to invoke this func
+   * @param {*} event 
+   */
+  onNameChange = (event) => {
+    this.setState({ userName: event.target.value})
+  }
+
   render() {
     return (
-      <div>
-        <h1>{this.state.title}</h1>
-        <img src={logo} onClick={this.onClick} 
-          alt="BridgeLabz Logo goes here."/>
-      </div>
+      <>
+        <div>
+          <h1>Hello {this.state.userName} from BridgeLabz</h1>
+          <img src={logo} onClick={this.onClick} 
+            alt="BridgeLabz Logo goes here."/>
+        </div>
+        <div>
+          <input onChange={this.onNameChange} />
+        </div>
+      </>
     )
   }
 } 
