c902f4efd6f1b28054bb7e3179100d76aa494c17
Sreenath285 <sreenath.285@gmail.com>
2022-04-24 19:22:30 +0530

    Displaying hello world using one way data binding

---
+++ helloworld/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      title: 'Hello from BridgeLabz'
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
