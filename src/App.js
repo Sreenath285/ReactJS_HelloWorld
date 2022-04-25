7fe841882f70c3e3d1fb3bac8c4d7e8c80abc972
Sreenath285 <sreenath.285@gmail.com>
2022-04-25 14:53:19 +0530

    Completed the hello from bridgelabz page with text

--- src/App.js
+++ src/App.js
@@ -16,14 +16,10 @@ class App extends React.Component {
   /**
    * onClick method is called when image is clicked
    */
-  onClick = ($event) => {
-    window.open(this.url,"_blank");
+  onClick = () => {
+    window.open(this.url);
   }
 
-  /**
-   * onChange event listener is used to invoke this func
-   * @param {*} event 
-   */
   onNameChange = (event) => {
     const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
     this.setState({ userName: event.target.value})
@@ -36,17 +32,31 @@ class App extends React.Component {
 
   render() {
     return (
-      <>
-        <div>
-          <h1>Hello {this.state.userName} from BridgeLabz</h1>
-          <img src={logo} onClick={this.onClick} 
+      <div class="page-content">
+        <h1>Hello {this.state.userName} from BridgeLabz</h1>
+        <img src={logo} onClick={this.onClick} 
             alt="BridgeLabz Logo goes here"/>
-        </div>
-        <div>
+        <div class="input-content">
           <input onChange={this.onNameChange} />
           <span className='error-output'>{this.state.nameError}</span>
         </div>
-      </>
+        <p>At BridgeLabz, we are a tech community of</p>
+        <ul>
+            <li>technologists</li>
+            <li>thinkers</li>
+            <li>builders</li>
+        </ul>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+            To know about us, visit <a href = {this.url} onClick={this.onClick}>BridgeLabz </a> 
+            to learn even more about our misiion i.e. <strong>Employability to all</strong>.
+        </p>  
+      </div>
     )
   }
 } 
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import logo from './assets/BL-logo.jpg';
+import React from 'react';
+import './App.css';
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div>
+                Home page
+            </div>
+        )
+    }
+}
+export default Home;
